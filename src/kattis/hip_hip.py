e4a51f1c5dec277af898235520a27e0df3ecc4f8
Hy <107110503+whanyu1212@users.noreply.github.com>
2024-08-03 14:09:28 +0000

    replacing the placeholder with some actual problem and its test

---
+++ src/kattis/hip_hip.py
@@ -0,0 +1,20 @@
+###################################################################################################
+# https://open.kattis.com/problems/hipphipp
+# Problem A - Hipp Hipp                                                                           #
+# Hipp Hipp                                                                                       #
+###################################################################################################
+
+# Forritunarkeppni Framhaldsskólanna was first held in the year 2001.
+# This year the contest is celebrating a big anniversary, being 20 years old!
+# Shouldn’t we congratulate?
+
+# Input
+# There is no input in this problem.
+
+# Output
+# Print 20 lines. Each line should contain Hipp hipp hurra!
+
+
+def hip_hip():
+    message = "Hipp hipp hurra!\n"
+    print(message * 20)
