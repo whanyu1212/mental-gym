04801670de6e41182ab306c4791f33a52f4d4685
Hanyu Wu <whanyu1212@hotmail.com>
2024-09-08 20:32:18 +0800

    restructured folders

--- src/dsa_from_scratch/list_adt/doubly_linked_list.py
+++ src/dsa_from_scratch/list_adt/doubly_linked_list.py
@@ -11,15 +11,15 @@ class ListNode:
     """Bidirectional linked list node class"""
 
     def __init__(self, data: int):
-        self.data = data
-        self.prev = None
-        self.next = None
+        self.data: int = data
+        self.prev: ListNode | None = None
+        self.next: ListNode | None = None
 
 
 class DoublyLinkedList:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head: DoublyLinkedList | None = None
+        self.tail: DoublyLinkedList | None = None
 
     def insert(self, data, position=None):
         new_node = ListNode(data)
