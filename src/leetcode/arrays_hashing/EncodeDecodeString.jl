bd567fe93418cc98eb1ad839f31fcae3cc5e497d
whanyu1212 <whanyu1212@hotmail.com>
2025-05-13 23:26:02 +0800

    adding encode and decode string in both python and julia

---
+++ src/leetcode/arrays_hashing/EncodeDecodeString.jl
@@ -0,0 +1,35 @@
+function encode(str::Vector{String})::String
+    result = ""
+    for s in str
+        result = result * string(length(s)) * '#' * s
+    end
+    return result
+end
+
+
+function decode(s::String)::Vector{String}
+    result = []
+    i = 1 # julia's index starts from 1 
+
+    while i <= length(s) # inclusive of the end
+        j = i
+        while s[j] != '#'
+            j += 1
+        end
+        length = parse(Int, s[i:j-1])
+        i = j + 1
+        j = i + length - 1
+        push!(result, s[i:j])
+        i = j + 1
+    end
+    return result
+
+end
+
+
+strs = ["neet", "code"]
+encoded_str = encode(strs)
+println("Encoded: ", encoded_str)
+
+decoded_strs = decode(encoded_str)
+println("Decoded: ", decoded_strs)
